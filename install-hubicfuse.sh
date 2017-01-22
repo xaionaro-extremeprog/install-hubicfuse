a002eee36059a09c8d4b89f993207ff0c7616cac
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2017-01-22 15:56:37 +0100

    Added autosetup

--- install-hubicfuse.sh
+++ install-hubicfuse.sh
@@ -56,24 +56,83 @@ function prepareDeb {
 		git push
 	fi
 
+	popd
+	popd
+
 	return 0
 }
 
+function installHubicFuse {
+	case "$DISTR" in
+		Ubuntu)
+			PKG_CATEGORY="$(echo $DISTR | tr '[:upper:]' '[:lower:]')"/"$VERSION"
+			prepareDeb "${PKG_CATEGORY}"
+			RC="$?"
+			if [ "$RC" -ne "0" ]; then
+				echo "An error occurred. Exit. Try: bash -x $0"
+				exit -1
+			fi
+			DEB="/tmp/install-hubicfuse/package/${PKG_CATEGORY}/cloudfuse.deb"
 
+			sudo dpkg -i "$DEB"
+			#rm -rf "/tmp/install-hubicfuse"
+			;;
+	esac
+}
 
-case "$DISTR" in
-	Ubuntu)
-		PKG_CATEGORY="$(echo $DISTR | tr '[:upper:]' '[:lower:]')"/"$VERSION"
-		prepareDeb "${PKG_CATEGORY}"
-		RC="$?"
-		if [ "$RC" -ne "0" ]; then
-			echo "An error occurred. Exit. Try: bash -x $0"
-			exit -1
-		fi
-		DEB="/tmp/install-hubicfuse/package/${PKG_CATEGORY}/cloudfuse.deb"
+function setupHubicFuse {
+	pushd /tmp
+	if ! [ -d "/tmp/install-hubicfuse/hubicfuse" ]; then
+		rm -rf "/tmp/install-hubicfuse"
+		git clone https://github.com/xaionaro-extremeprog/install-hubicfuse
+		pushd install-hubicfuse
+		git submodule update --init --recursive
+		popd
+	else
+		pushd "/tmp/install-hubicfuse"
+		git submodule update --init --recursive
+		popd
+	fi
+	popd
 
-		sudo dpkg -i "$DEB"
-		#rm -rf "/tmp/install-hubicfuse"
-		;;
-esac
+	pushd /tmp/install-hubicfuse/hubicfuse
+
+	export client_id="${HUBIC_CLIENT_ID}"
+	export client_secret="${HUBIC_CLIENT_SECRET}"
+	export user_login="${HUBIC_USER_LOGIN}"
+	export user_pwd="${HUBIC_USER_PASSWORD}"
+	export redirect_uri="${HUBIC_REDIRECT_URI:-http://localhost/}"
+	export usage="${HUBIC_USAGE:-r}"
+	export getAllLinks="${HUBIC_GETALLLINKS:-r}"
+	export credentials="${HUBIC_credentials:-r}"
+	export activate="${HUBIC_ACTIVATE:-}"
+	export links="${HUBIC_LINKS:-}"
+
+	# see source code of "hubic_token":
+	scope='account.r'
+	[ "$usage" = 'r' ] && scope="${scope},usage.r"
+	[ "$getAllLinks" = 'r' ] && scope="${scope},getAllLinks.r"
+	[ "$credentials" = 'r' ] && scope="${scope},credentials.r"
+	[ "$activate" = 'w' ] && scope="${scope},activate.w"
+	l="$( printf -- '%s' "${links}" | sed 's/[^\(w\|r\|d\)]//g' )"
+	[ -n "$l" ] && [ "${l}" = "${links}" ] && scope="${scope},links.${l}"
+
+	export scope
+
+	./hubic_token | grep -A 31415 "^#" > ~/.hubicfuse
+	#sed -e 's/read -p/echo/g' hubic_token | bash
+
+	popd
+}
+
+for var in HUBIC_CLIENT_ID HUBIC_CLIENT_SECRET HUBIC_USER_LOGIN; do
+	eval "varValue=\"\$$var\""
+	if [ "$varValue" = "" ]; then
+		echo "Variables HUBIC_CLIENT_ID, HUBIC_CLIENT_SECRET and HUBIC_USER_LOGIN must be set before this script. Optionally you can also set HUBIC_USER_PASSWORD." >&2
+		exit 1
+	fi
+done
+
+installHubicFuse
+setupHubicFuse
 
